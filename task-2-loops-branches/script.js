1fc92aab19f59baaf29fc1ce5cfbc1a9900ca827
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-06-04 12:41:18 +0300

    fixed validation

--- task-2-loops-branches/script.js
+++ task-2-loops-branches/script.js
@@ -1,14 +1,17 @@
-let n = +prompt('Введите число n',0);
+let n = Math.trunc(+prompt('Введите число n',0));
 
-while(isNaN(n)) n = +prompt('n не является числом, попробуйте ещё раз',0);
+while(isNaN(n)) n = Math.trunc(+prompt('n не является числом, попробуйте ещё раз',0));
 
-let m = +prompt('Введите число m', 0);
+let m = Math.trunc(+prompt('Введите число m', 0));
 
-while(isNaN(m)) m = +prompt('m не является числом, попробуйте ещё раз', 0);
+while(isNaN(m)) m = Math.trunc(+prompt('m не является числом, попробуйте ещё раз', 0));
+
+let min = Math.min(m,n);
+let max = Math.max(m,n);
 
 let mode = confirm('Пропускать парные числа?');
 let sum = 0;
-for (let i = n; i <= m; i++){
+for (let i = min; i <= max; i++){
     if (mode && i % 2 == 0) continue;
     sum+=i;
 }
